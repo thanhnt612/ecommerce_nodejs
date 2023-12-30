43d8d42f34b288807885d899375ae2e6738cf119
thanhnt612 <nguyentruongthanh0612@gmail.com>
2023-12-30 16:15:09 +0700

    change node version

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -7,9 +7,10 @@ const {
     DeleteObjectCommand
 } = require("../configs/s3.config")
 const crypto = require('crypto')
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+// const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 const randomImageName = () => crypto.randomBytes(16).toString('hex')
-
+const urlImagePublic = `https://d1231bv564e1.cloudfront.net`
 const uploadImageFromS3 = async ({ file }) => {
     try {
         const imageName = randomImageName()
@@ -20,12 +21,23 @@ const uploadImageFromS3 = async ({ file }) => {
             ContentType: 'image/jpeg'
         })
         const result = await s3.send(command)
-        const signedUrl = new GetObjectCommand({
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: imageName,
+
+        // const signedUrl = new GetObjectCommand({
+        //     Bucket: process.env.AWS_BUCKET_NAME,
+        //     Key: imageName,
+        // })
+
+        // const url = await getSignedUrl(s3, signedUrl, { expiresIn: 3600 });
+        const url = getSignedUrl({
+            url: `${urlImagePublic}/${imageName}`,
+            keyPairId: process.env.AWS_CLOUDFRONT_PUBLIC_KEY_ID,
+            dateLessThan: new Date(Date.now() + 1000 * 60),
+            privateKey: process.env.AWS_CLOUDFRONT_PRIVATE_KEY_ID,
         })
-        const url = await getSignedUrl(s3, signedUrl, { expiresIn: 3600 });
-        return url
+        return {
+            url,
+            result
+        }
     } catch (error) {
         console.error(`Error upload image S3`, error)
     }
